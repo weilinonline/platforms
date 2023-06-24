b428d02fe372fe4ccde0f3f1785208bdd3057502
weilinonline <weilinonline@163.com>
2023-06-24 22:19:37 +0800

    refactor

--- lib/api/post.ts
+++ lib/api/post.ts
@@ -3,7 +3,7 @@ import prisma from "@/lib/prisma";
 import { NextApiRequest, NextApiResponse } from "next";
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "pages/api/auth/[...nextauth]";
-import type { Post, Site } from ".prisma/client";
+import type { Post, Site } from "@prisma/client";
 import type { Session } from "next-auth";
 import { revalidate } from "@/lib/revalidate";
 import { getBlurDataURL, placeholderBlurhash } from "@/lib/utils";
